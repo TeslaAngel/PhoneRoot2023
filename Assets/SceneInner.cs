3c26ed11132d9925b3a94c8ce1876a6fe5a6c51b
TeslaAngel <3363154242@qq.com>
2023-02-04 13:53:05 +0800

    using 3d teshmeshpro

---
+++ Assets/SceneInner.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class SceneInner : MonoBehaviour
+{
+    public float SSTime;
+
+    private float SSedTime;
+    private SpriteRenderer spriteRenderer;
+
+
+    void Awake()
+    {
+        SSedTime = SSTime;
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        SSedTime -= Time.deltaTime;
+        if (SSedTime > 0)
+        {
+            spriteRenderer.color = new Color(spriteRenderer.color.r, spriteRenderer.color.g, spriteRenderer.color.b, SSedTime / SSTime);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
+    }
+}
